ddf38edabc38491c753f2f56376fa15ca785b78c
LucasTecDev <lucas.henry71m@gmail.com>
2023-12-15 11:29:27 -0300

    Programando o Layout do Stateful Widget de Perguntas e Respostas

--- app_quiz/lib/main.dart
+++ app_quiz/lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:app_quiz/quiz.dart';
 import 'package:flutter/material.dart';
 import 'package:app_quiz/homepage.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(Quiz());
 }
 
---
+++ app_quiz/lib/quiz.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('                              Quiz'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Text('Pergunta 1 de 10', style: TextStyle(fontSize: 20))),
+                 Text('Pergunta',style: TextStyle(fontSize: 20)),
+        
+                ElevatedButton(
+                  onPressed: () {
+                    print('1');
+                  },
+                  child: Text('Resposta 1', style: TextStyle(fontSize: 20)),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.fromLTRB(60, 12, 60, 12),
+                  ),
+                ),
+        
+                ElevatedButton(
+                  onPressed: () {
+                    print('2');
+                  },
+                  child: Text('Resposta 2', style: TextStyle(fontSize: 20)),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.fromLTRB(60, 12, 60, 12),
+                  ),
+                ),
+        
+                ElevatedButton(
+                  onPressed: () {
+                    print('3');
+                  },
+                  child: Text('Resposta 3', style: TextStyle(fontSize: 20)),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.fromLTRB(60, 12, 60, 12),
+                  ),
+                ),
+        
+                ElevatedButton(
+                  onPressed: () {
+                    print('4');
+                  },
+                  child: Text('Resposta 4', style: TextStyle(fontSize: 20)),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.fromLTRB(60, 12, 60, 12),
+                  ),
+                ),
+        
+              ],
+              
+            ),
+          ),
+        ),
+      );
+  }
+}
+
